48ab6d784211a7bb5079502b0450dd566a8c7c00
tiagoantonio <tiagoantonio@gmail.com>
2016-03-21 16:04:03 -0300

    parallel fork fixed

--- woland-batch.pl
+++ woland-batch.pl
@@ -32,11 +32,14 @@ my @time;
 my ($profile, $hotspot, $genomeVersion);
 my ($pm,$pid, $MAX_PROCESSES);
 my @ARGS;
+my @time1;
 
 ## main warning
 unless (@ARGV){
 	die "\nERROR : Usage: $0 <input.table> <chromosome_profile_file> <hotspot_window_length> <genome_version> \n";	
 }
+
+@time=localtime; #time characters for analysis folder name
 
 ## parsing input table
 $inputTable = $ARGV[0]; #<input_table>
@@ -53,7 +56,6 @@ foreach $inputTableLine (@inputTableArray){ #two arrays for each category (group
 @i=();
 $i=0;
 
-@time=localtime; #time characters for analysis folder name
 
 mkdir("results-batch-$inputTable-$time[0].$time[1].$time[2].$time[3].$time[4].$time[5]", 0755) || die "Cannot create results folder - check if it already exists";
 mkdir("results-batch-$inputTable-$time[0].$time[1].$time[2].$time[3].$time[4].$time[5]/samples-$inputTable", 0755) || die "Cannot create results folder- check if it already exists";
@@ -63,23 +65,24 @@ $profile= $ARGV[1]; #chromosome profile <chromosome_length_profile>
 $hotspot= $ARGV[2]; #natural number for hotspot window <hotspot_window_length> 
 $genomeVersion = $ARGV[3]; #genome version as in genomes/genome_<genome_version>.fa and genomes/refseq_<genome_version>.txt
 
+$pm = Parallel::ForkManager->new(30);
+
+
 for my $i (0..$#sampleName){ #execution of woland-anno.pl for each sample
+	@ARGS=();
 	push (@ARGS, $sampleName[$i]);
 	push (@ARGS, $profile);
 	push (@ARGS, $hotspot);
 	push (@ARGS, $genomeVersion);
-	
-	$pm = Parallel::ForkManager->new($MAX_PROCESSES);
+
 	$pid=$pm->start and next;
 	system ($^X, "woland-anno.pl", @ARGS);
 	$pm->finish;
-	@ARGS=();
 }
 
-$pm = Parallel::ForkManager->new($MAX_PROCESSES); #execution of woland-report.pl
-$pid=$pm-> start and next;
+$pm->wait_all_children;
+
 system ($^X, "woland-report.pl", $ARGV[0]);
-$pm->finish;
 
 # moving report folder and files to results-batch
 move ("report-$inputTable", "results-batch-$inputTable-$time[0].$time[1].$time[2].$time[3].$time[4].$time[5]/report-$inputTable");
@@ -88,5 +91,10 @@ move ("report-$inputTable", "results-batch-$inputTable-$time[0].$time[1].$time[2
 for my $i (0..$#inputTableArray){
 	move ("results-$sampleName[$i]", "results-batch-$inputTable-$time[0].$time[1].$time[2].$time[3].$time[4].$time[5]/samples-$inputTable/results-$sampleName[$i]");
 }
+
+@time1=localtime;
+
+print "@time\n";
+print "@time1\n";
 
 exit;
